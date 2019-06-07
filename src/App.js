e7599a7e322a4d01a16c33fc741fb2cd7afb3419
Matthew Wiener <mw52588@gmail.com>
2019-06-07 18:26:32 -0400

    Added styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './weather.png';
 import './App.css';
 
 class App extends Component {
@@ -7,8 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={logo} className="App-logo" alt="weather" />
+          <h1>Weather Application</h1>
+          <form>
+            <input className="App-header-searchbar" type="string" />
+          </form>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
